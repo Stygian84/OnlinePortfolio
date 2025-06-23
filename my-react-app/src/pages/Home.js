e8dcfa20329469a9f70af365f86e1e760b944f7f
Stygian84 <nicholasgandhi84@gmail.com>
2025-06-24 02:01:04 +0800

    uncomment code

--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -589,12 +589,12 @@ function HomeContent() {
         </div>
 
         {/* Youtube */}
-        {/* <div>
+        <div>
           <div ref={gridRefHeader2} id="youtube"></div>
           <Divider sx={{ ...dividerStyles }} className={headerVisible2 ? "bounce-from-below" : ""}>
             <YouTubeDLContent />
           </Divider>
-        </div> */}
+        </div>
         <MatrixAnimation />
       </div>
     </div>
